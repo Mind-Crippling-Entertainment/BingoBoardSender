6879c42de016a53fc1a13c3a11dd5e1db81704b5
The009 <the009@gmail.com>
2024-06-12 16:03:15 -0400

    Update main.cpp
    Make it work!
    Makes a working alpha sender.
    ( It says sending, Now to fix the  rec. )

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,8 @@ bool keyPressed = false; //status which keeps track if a key was pressed
 void lcdDisplay(String, String, String, String);
 void ScreenSaver(void);
 void KeyPad(void);
-void ManageData(char *);
+void ManageData(char);
+void Append(char* s, char c);
 
 
 uint8_t broadcastAddress[] = REMOTEMAC;
@@ -42,10 +43,12 @@ unsigned long timerDelay = 2000;  // send readings timer
 void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
   Serial.print("Last Packet Send Status: ");
   if (sendStatus == 0){
+    lcdDisplay("Current Time: ", "<time>", "Sent Data Status: ", "Delivery success");
     Serial.println("Delivery success");
   }
   else{
     Serial.println("Delivery fail");
+    lcdDisplay("Current Time: ", "<time>", "Sent Data Status:" , "Delivery FAIL");
   }
 }
  
@@ -129,7 +132,7 @@ void loop() {
 {
   if (keyPressed == true)
   {
-    if (millis() - KeyTimer > 100)
+    if (millis() - KeyTimer > 350)
     {
       KeyTimer = millis();
       keyPressed = false;
@@ -149,22 +152,22 @@ void loop() {
     switch (Wire.read())
     {
       case 238: //Key D
-      ManageData((char *)'D');
+      ManageData('D');
       keyPressed = true;
       break;
   
       case 237: //Key #
-      ManageData((char *)'#');
+      ManageData('#');
       keyPressed = true;
       break;
      
       case 235: //Key 0
-      ManageData((char *)'0');
+      ManageData('0');
       keyPressed = true;
       break;
      
       case 231: //Key *
-      ManageData((char *)'*');
+      ManageData('*');
       keyPressed = true;
       break;
     }
@@ -178,22 +181,22 @@ void loop() {
     switch (Wire.read())
     {
       case 222: //Key C
-        ManageData((char *)'C');
+        ManageData('C');
         keyPressed = true;
       break;
 
       case 221: //Key 9
-        ManageData((char *)'9');
+        ManageData('9');
         keyPressed = true;
       break;
 
       case 219: //Key 8
-        ManageData((char *)'8');
+        ManageData('8');
         keyPressed = true;
       break;
 
       case 215: //Key 7
-        ManageData((char *)'7');
+        ManageData('7');
         keyPressed = true;
       break;
     }
@@ -206,21 +209,21 @@ void loop() {
     switch (Wire.read())
     {
       case 190: //Key B
-        ManageData((char *)'B');
+        ManageData('B');
       break;
 
       case 189: //Key 6
-        ManageData((char *)'6');
+        ManageData('6');
         keyPressed = true;
       break;
 
       case 187: //Key 5
-        ManageData((char *)'5');
+        ManageData('5');
         keyPressed = true;
       break;
 
       case 183: //Key 4
-        ManageData((char *)'4');
+        ManageData('4');
         keyPressed = true;
       break;
     }
@@ -233,22 +236,22 @@ void loop() {
     switch (Wire.read())
     {
       case 126: //Key A
-        ManageData((char *)'A');
+        ManageData('A');
         keyPressed = true;
       break;
 
       case 125: //Key 3
-      ManageData((char *)'3');
+      ManageData('3');
       keyPressed = true;
       break;
 
       case 123: //Key 2
-        ManageData((char *)'2');
+        ManageData('2');
         keyPressed = true;
       break;
 
       case 119: //Key 1
-        ManageData((char *)'1');
+        ManageData('1');
         keyPressed = true;
       break;
     }
@@ -256,20 +259,30 @@ void loop() {
   }
 }
 
-void ManageData(char * key){
+void ManageData(char key){
 
- switch (*key)
+ switch (key)
  {
    case '*' :
-    memset(keyConcat, 0, 32); 
+    memset(keyConcat, 0, 32);
+    Serial.println("Cleared");
+    lcdDisplay("Current Time: ", "<time>", "Data Cleared: ", keyConcat);
   break;
 
   case '#' :
+    Serial.print("Sending: ");
+    Serial.println(keyConcat);
     SendData(keyConcat);
+    memset(keyConcat, 0, 32);
+    lcdDisplay("Current Time: ", "<time>", "Sent Data: ", keyConcat);
   break;
 
  default:
-     strcat(keyConcat, key);
+    Serial.write("Key: " + key);
+    char tmpKey[3] = {key};
+    strcat(keyConcat, tmpKey );
+    Serial.println(keyConcat);
+    lcdDisplay("Current Time: ", "<time>", "Data To Send: ", keyConcat);
   break;
  }
 }
